c85d27d80fed52a3e9457cd5172f40c9d546ace3
NoName <mostblack4@gmail.com>
2026-02-04 01:38:29 +0300

    feat(login-page): create component

---
+++ src/pages/login/ui/login-page.tsx
@@ -0,0 +1,9 @@
+import { LoginWidget } from "@/widgets/auth/login-widget/ui/login-widget";
+
+export function LoginPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <LoginWidget />
+    </main>
+  );
+}
