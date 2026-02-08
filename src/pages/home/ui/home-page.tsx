7609fea4ef2bd07380456f0bdf4420b2f3088842
NoName <mostblack4@gmail.com>
2026-02-09 01:31:16 +0300

    refactor(home-page): removed main tag

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -5,9 +5,5 @@ import { ProductListWidget } from "@/widgets/product-list/ui/product-list";
 export const HomePage = async () => {
   const { products } = await getProductsList({ page: 1, limit: LIMIT });
 
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <ProductListWidget initialProducts={products} />
-    </main>
-  );
+  return <ProductListWidget initialProducts={products} />
 };
