10629262cc89644a5ce6cd2eb143fe52f563600f
NoName <mostblack4@gmail.com>
2026-02-12 02:16:27 +0300

    feat(cart-page): add server components in widget props

--- src/pages/cart/ui/cart-page.tsx
+++ src/pages/cart/ui/cart-page.tsx
@@ -1,11 +1,9 @@
 import { EmptyCart } from "@/entities/cart";
 import { CartSkeleton } from "@/entities/cart";
-import { getCurrentUser } from "@/shared/api/auth";
 import { CartWidget } from "@/widgets/cart";
 
-export const CartPage = async () => {
-  const user = await getCurrentUser()
+export const CartPage = () => {
 
-  return <CartWidget userId={user?.id ?? null} emptySlot={<EmptyCart />} skeleton={<CartSkeleton />} />
+  return <CartWidget emptySlot={<EmptyCart />} skeleton={<CartSkeleton />} />
 
 }
